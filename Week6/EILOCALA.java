39a69514f9c874e5a54179e29707191847cb5f53
eiuthanghuynh <thang.huynhle.cit22@eiu.edu.vn>
2025-11-27 10:45:42 +0700

    W6 Finalize: Add all week 6 problems

---
+++ Week6/EILOCALA.java
@@ -0,0 +1,170 @@
+package Week6;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class EILOCALA {
+    public static void main(String[] args) {
+        int n = ni();
+        Vertex[] vertices = new Vertex[n];
+        for (int i = 0; i < n; i++) {
+            vertices[i] = new Vertex(i);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int u = ni();
+            int v = ni();
+            int w = ni();
+            vertices[u].edges.add(new Edge(vertices[v], w));
+            vertices[v].edges.add(new Edge(vertices[u], w));
+        }
+        dfs(vertices[0]);
+        int temp = maxVertexId;
+        for (int i = 0; i < n; i++) {
+            vertices[i].isDiscover = false;
+            vertices[i].lengthFromRoot = 0;
+        }
+        maxLength = -1;
+        maxVertexId = -1;
+        dfs(vertices[temp]);
+        System.out.println(Math.min(temp, maxVertexId) + " " + maxLength);
+    }
+
+    static int maxLength = -1;
+    static int maxVertexId = -1;
+
+    public static void dfs(Vertex v) {
+        v.isDiscover = true;
+        if (v.lengthFromRoot > maxLength) {
+            maxLength = v.lengthFromRoot;
+            maxVertexId = v.id;
+        } else if (v.lengthFromRoot == maxLength && v.id < maxVertexId) {
+            maxVertexId = v.id;
+        }
+        for (Edge e : v.edges) {
+            if (!e.endVertex.isDiscover) {
+                e.endVertex.lengthFromRoot = v.lengthFromRoot + e.weight;
+                dfs(e.endVertex);
+            }
+        }
+    }
+
+    static class Vertex {
+        int id;
+        boolean isDiscover = false;
+        int lengthFromRoot = 0;
+        ArrayList<Edge> edges = new ArrayList<>();
+
+        public Vertex(int id) {
+            this.id = id;
+        }
+    }
+
+    static class Edge {
+        Vertex endVertex;
+        int weight;
+
+        public Edge(Vertex endVertex, int weight) {
+            this.endVertex = endVertex;
+            this.weight = weight;
+        }
+    }
+
+    static InputStream is = System.in;
+    static byte[] inbuf = new byte[1 << 24];
+    static int lenbuf = 0, ptrbuf = 0;
+
+    static int readByte() {
+        if (lenbuf == -1)
+            throw new InputMismatchException();
+        if (ptrbuf >= lenbuf) {
+            ptrbuf = 0;
+            try {
+                lenbuf = is.read(inbuf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (lenbuf <= 0)
+                return -1;
+        }
+        return inbuf[ptrbuf++];
+    }
+
+    static boolean isSpaceChar(int c) {
+        return !(c >= 33 && c <= 126);
+    }
+
+    static int skip() {
+        int b;
+        while ((b = readByte()) != -1 && isSpaceChar(b))
+            ;
+        return b;
+    }
+
+    static double nd() {
+        return Double.parseDouble(ns());
+    }
+
+    static char nc() {
+        return (char) skip();
+    }
+
+    static String ns() {
+        int b = skip();
+        StringBuilder sb = new StringBuilder();
+        while (!(isSpaceChar(b))) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    static char[] ns(int n) {
+        char[] buf = new char[n];
+        int b = skip(), p = 0;
+        while (p < n && !(isSpaceChar(b))) {
+            buf[p++] = (char) b;
+            b = readByte();
+        }
+        return n == p ? buf : Arrays.copyOf(buf, p);
+    }
+
+    static int ni() {
+        int num = 0, b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+            ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+
+    static long nl() {
+        long num = 0;
+        int b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+            ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+}
---
+++ Week6/WTRABS.java
@@ -0,0 +1,164 @@
+package Week6;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class WTRABS {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        int n = ni();
+        Vertex[] vertices = new Vertex[n];
+        for (int i = 0; i < n; i++) {
+            double w = nd();
+            vertices[i] = new Vertex(i, w);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int u = ni();
+            int v = ni();
+            vertices[v].addNeighbor(vertices[u]);
+        }
+        bfs(vertices[0]);
+        for (Vertex vertex : vertices) {
+            if (vertex.isLeaf) {
+                sb.append(vertex.id).append(" ").append(Math.round(vertex.water * 10000.0) / 10000.0).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static void bfs(Vertex v) {
+        Queue<Vertex> q = new ArrayDeque<Vertex>();
+        q.add(v);
+        v.isDiscover = true;
+        while (!q.isEmpty()) {
+            Vertex w = q.poll();
+            if (w.neighbors.size() == 0) {
+                w.isLeaf = true;
+                continue;
+            }
+            for (Vertex x : w.neighbors) {
+                if (!x.isDiscover) {
+                    x.isDiscover = true;
+                    x.water += w.water / w.neighbors.size();
+                    q.add(x);
+                }
+            }
+        }
+    }
+
+    static class Vertex {
+        int id;
+        double water;
+        boolean isDiscover = false;
+        boolean isLeaf = false;
+        ArrayList<Vertex> neighbors = new ArrayList<>();
+
+        public Vertex(int id, double w) {
+            this.id = id;
+            this.water = w;
+        }
+
+        public void addNeighbor(Vertex v) {
+            neighbors.add(v);
+        }
+    }
+
+    static InputStream is = System.in;
+    static byte[] inbuf = new byte[1 << 24];
+    static int lenbuf = 0, ptrbuf = 0;
+
+    static int readByte() {
+        if (lenbuf == -1)
+            throw new InputMismatchException();
+        if (ptrbuf >= lenbuf) {
+            ptrbuf = 0;
+            try {
+                lenbuf = is.read(inbuf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (lenbuf <= 0)
+                return -1;
+        }
+        return inbuf[ptrbuf++];
+    }
+
+    static boolean isSpaceChar(int c) {
+        return !(c >= 33 && c <= 126);
+    }
+
+    static int skip() {
+        int b;
+        while ((b = readByte()) != -1 && isSpaceChar(b))
+            ;
+        return b;
+    }
+
+    static double nd() {
+        return Double.parseDouble(ns());
+    }
+
+    static char nc() {
+        return (char) skip();
+    }
+
+    static String ns() {
+        int b = skip();
+        StringBuilder sb = new StringBuilder();
+        while (!(isSpaceChar(b))) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    static char[] ns(int n) {
+        char[] buf = new char[n];
+        int b = skip(), p = 0;
+        while (p < n && !(isSpaceChar(b))) {
+            buf[p++] = (char) b;
+            b = readByte();
+        }
+        return n == p ? buf : Arrays.copyOf(buf, p);
+    }
+
+    static int ni() {
+        int num = 0, b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+            ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+
+    static long nl() {
+        long num = 0;
+        int b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+            ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+}
